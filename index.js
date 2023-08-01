963a7ce71228993f8e08150c197660d14326901e
dianasul <sulkova.diana@gmail.com>
2023-08-01 12:54:36 +0200

    Added Font awesome CDN, icons, their data attributes

--- index.js
+++ index.js
@@ -5,25 +5,28 @@ function getFeedHtml() {
 	tweetsData.forEach((tweet) => {
 		feedHtml +=`
 		<div class="tweet">
-    <div class="tweet-inner">
-        <img src="${tweet.profilePic}" class="profile-pic">
-        <div>
-            <p class="handle">${tweet.handle}</p>
-            <p class="tweet-text">${tweet.tweetText}</p>
-            <div class="tweet-details">
-                <span class="tweet-detail">
-                    ${tweet.replies.length}
-                </span>
-                <span class="tweet-detail">
-                    ${tweet.likes}
-                </span>
-                <span class="tweet-detail">
-                    ${tweet.retweets}
-                </span>
-            </div>   
-        </div>            
-    </div>
-</div>`;
+			<div class="tweet-inner">
+				<img src="${tweet.profilePic}" class="profile-pic">
+				<div>
+					<p class="handle">${tweet.handle}</p>
+					<p class="tweet-text">${tweet.tweetText}</p>
+						<div class="tweet-details">
+							<span class="tweet-detail">
+								<i class="fa-regular fa-comment-dots" data-reply="${tweet.uuid}"></i>
+								${tweet.replies.length}
+							</span>
+							<span class="tweet-detail">
+								<i class="fa-solid fa-heart" data-like="${tweet.uuid}"></i>
+								${tweet.likes}
+							</span>
+							<span class="tweet-detail">
+								<i class="fa-solid fa-retweet" data-retweet="${tweet.uuid}"></i>
+								${tweet.retweets}
+							</span>
+						</div>   
+				</div>            
+			</div>"
+		</div>`;
 
 	})
 return feedHtml
